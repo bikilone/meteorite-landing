1bfad6bda7abb2c26ce85ca8180d709992086495
bikilone <milosbirovljevic@gmail.com>
2019-05-24 11:59:52 +0200

    Updates

---
+++ precache-manifest.4d60e5cd4274ea051dd98b89cf4cef7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd972531aa5e42792d4da550e24606ce",
+    "url": "/meteorite-landing/index.html"
+  },
+  {
+    "revision": "24c08241603629de0598",
+    "url": "/meteorite-landing/static/css/main.6c85d1e9.chunk.css"
+  },
+  {
+    "revision": "e13f7d7ae0d9e713894e",
+    "url": "/meteorite-landing/static/js/2.3c353089.chunk.js"
+  },
+  {
+    "revision": "24c08241603629de0598",
+    "url": "/meteorite-landing/static/js/main.3a0855c4.chunk.js"
+  },
+  {
+    "revision": "5e6d3936113ad2dc6ba2",
+    "url": "/meteorite-landing/static/js/runtime~main.c6e2d7fd.js"
+  }
+]);
